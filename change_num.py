266a0ec97784198068b9cda06e97aed90d06caee
Victor7004 <viktoraleksejcuk@gmail.com>
2021-09-09 20:14:29 +0300

    Update change_num.py

--- change_num.py
+++ change_num.py
@@ -3,6 +3,7 @@ numbers = input().split()
 #через цикл перебираем все пары значений строки по индексу и меняем их местами
 #длина шага 2 от 0 до длины numbers
 for i in range(0, len(numbers) - 1, 2):
+    #меняем значение элементов пары вблизи стоящих чисел
     numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
 #распаковуем строку в принте    
 print(*numbers)
