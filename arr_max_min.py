2edfec738bc805e4c33f959a23339a8d4d912bec
Victor7004 <viktoraleksejcuk@gmail.com>
2021-12-23 16:38:08 +0200

    Create arr_max_min.py

---
+++ arr_max_min.py
@@ -0,0 +1,12 @@
+my_list = [[12, 221, 3], [41, 5, 633], [71, 8, 99]]
+
+maximum = my_list[0][0]
+minimum = my_list[0][0]
+
+for row in my_list:
+    maximum = max(row)
+    minimum = min(row)
+
+print(maximum + minimum)
+
+# Выведет 107 (99 + 8)
