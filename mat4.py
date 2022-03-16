3058d65bd11a8ae9a6a6dbfecd998f5e19b87f50
Victor7004 <viktoraleksejcuk@gmail.com>
2022-03-16 14:27:35 +0200

    Update mat4.py

--- mat4.py
+++ mat4.py
@@ -11,3 +11,18 @@ for i in range(n):
         mat[i] = mat[i][::-1]           
 for el in mat:
     print(*el)
+
+# Верное решение #474082930
+n, m = [int(i) for i in input().split()]
+matrix = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        matrix[i][j] = i * m + j + 1
+    if i % 2:
+        matrix[i].reverse()
+
+for i in range(n):
+    for j in range(m):
+        print(str(matrix[i][j]).ljust(3), end=' ')
+    print()    
