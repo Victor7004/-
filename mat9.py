29f1118b693600673909bd493b8fb50a7a348147
Victor7004 <viktoraleksejcuk@gmail.com>
2022-03-18 16:33:47 +0200

    Update mat9.py

--- mat9.py
+++ mat9.py
@@ -28,3 +28,19 @@ for i in range( n):
 #print(C)
 for el in C:
     print(*el)
+
+# Верное решение #469420019
+n, m = [int(i) for i in input().split()]
+matrixA = [[int(i) for i in input().split()] for _ in range(n)]
+input()
+m, k = [int(i) for i in input().split()]
+matrixB = [[int(i) for i in input().split()] for _ in range(m)]
+matrixC = [[0] * k for _ in range(n)]
+
+for i in range(n):
+    for j in range(k):
+        for q in range(m):
+            matrixC[i][j] += matrixA[i][q] * matrixB[q][j]
+
+for row in matrixC:
+    print(*row)
