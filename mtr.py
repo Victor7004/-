2a9a7add5ea5030d599718d990e9ad31eedfccd6
Victor7004 <viktoraleksejcuk@gmail.com>
2022-03-31 14:40:24 +0300

    Create mtr.py

---
+++ mtr.py
@@ -0,0 +1,14 @@
+x, y = input()
+n = 8
+board = [['.'] * n for _ in range(n)]
+x = ord(x) - 97
+y = n - int(y)
+board[y][x] = 'N'
+
+for i in range(n):
+    for j in range(n):
+        if abs(y - i) * abs(x - j) == 2:
+            board[i][j] = '*'
+        
+for row in board:
+    print(*row)
