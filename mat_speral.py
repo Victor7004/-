9c05446b7eb2fc166810436e5b634fc765a7ed9e
Victor7004 <viktoraleksejcuk@gmail.com>
2022-04-05 15:33:46 +0300

    Create mat_speral.py

---
+++ mat_speral.py
@@ -0,0 +1,42 @@
+# Заполнение спиралью 😈😈
+# На вход программе подаются два натуральных числа nn и mm. 
+# Напишите программу, которая создает матрицу размером n \times mn×m заполнив её "спиралью" в соответствии с образцом.
+
+n, m = [int(i) for i in input().split()]
+
+matrix = [[0] * m for _ in range(n)]
+counter = 1
+rows_passed, columns_passed = 0, 0
+current_row, current_column = 0, 0
+
+for k in range(n * m):
+    if counter == n * m + 1:
+        break
+    direction = k % 4
+    if direction == 0:
+        for j in range(columns_passed, m - columns_passed):
+            matrix[current_row][j] = counter
+            counter += 1
+        current_column = j
+        rows_passed += 1
+    elif direction == 1:
+        for i in range(rows_passed, n - rows_passed + 1):
+            matrix[i][current_column] = counter
+            counter += 1
+        current_row = i
+        columns_passed += 1
+    elif direction == 2:
+        for j in range(current_column - 1, columns_passed - 2, -1):
+            matrix[current_row][j] = counter 
+            counter += 1
+        current_column = j
+    elif direction == 3:
+        for i in range(current_row - 1, rows_passed - 1, -1):
+            matrix[i][current_column] = counter
+            counter += 1
+        current_row = i 
+        
+for i in range(n):
+    for j in range(m):
+        print(matrix[i][j], end=' ')
+    print()
