a071e85d529fb4ffde9005fe42c1af3a3dcc50ff
Victor7004 <viktoraleksejcuk@gmail.com>
2022-04-22 14:42:46 +0300

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,4 +1,5 @@
 # Дополните приведенный код, используя индексацию кортежа, чтобы переменная last, содержала последний элемент кортежа countries.
+
 countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
 last = countries[6]
 print(last)
