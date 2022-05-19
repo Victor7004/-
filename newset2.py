ae54bfecc89fbb4ed7588c2f07373113ac4cb0f6
Victor7004 <viktoraleksejcuk@gmail.com>
2022-05-19 13:53:49 +0300

    Update newset2.py

--- newset2.py
+++ newset2.py
@@ -1,3 +1,12 @@
 # На вход программе подаются два числа. Напишите программу, определяющую, есть ли в данных числах одинаковые цифры.
 
 print('NO' if set(input()).isdisjoint(set(input())) else 'YES')
+
+# Альтернативный вариант
+a = set(input())
+b = set(input())
+c = a.intersection(b)
+if len(c) > 0:
+    print('YES')
+else:
+    print('NO')
