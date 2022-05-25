c837a624203ba1b106f18591c65a43e228a4b984
Victor7004 <viktoraleksejcuk@gmail.com>
2022-05-25 12:25:19 +0300

    Update new_set3.py

--- new_set3.py
+++ new_set3.py
@@ -4,3 +4,10 @@ set1 = set(int(i) for i in input().split())
 set2 = set(int(i) for i in input().split())
 set3 = set(int(i) for i in input().split())
 print(*sorted((set1 | set2 | set3)-(set1 | set2), reverse = True))
+
+# 
+set1 = set(int(i) for i in input().split())
+set2 = set(int(i) for i in input().split())
+set3 = set(int(i) for i in input().split())
+
+print(*sorted(set3 - set2 - set1, reverse=True))
