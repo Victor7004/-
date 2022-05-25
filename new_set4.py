6d1c3d7efbbfe5322e28ce1bde26d32031778a7f
Victor7004 <viktoraleksejcuk@gmail.com>
2022-05-25 16:44:06 +0300

    Update new_set4.py

--- new_set4.py
+++ new_set4.py
@@ -8,3 +8,11 @@ for i in range(11):
     set4.add(i)
 #print(set4)
 print(*sorted((set4)-(set1 | set2 | set3)))
+
+# Верное решение #469405777
+# Python 3
+set1 = set(int(i) for i in input().split())
+set2 = set(int(i) for i in input().split())
+set3 = set(int(i) for i in input().split())
+
+print(*sorted(set(range(11)) - set1 - set2 - set3))
