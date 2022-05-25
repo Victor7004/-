47a498fd292c7fbd845e04bf4b4f1a9e5934d79c
Victor7004 <viktoraleksejcuk@gmail.com>
2022-05-25 12:24:02 +0300

    Create new_set3.py

---
+++ new_set3.py
@@ -0,0 +1,6 @@
+# Даны по 1010-балльной шкале оценки по физике трех учеников. 
+# Напишите программу, которая выводит множество оценок третьего ученика, которые не встречаются ни у первого, ни у второго ученика
+set1 = set(int(i) for i in input().split())
+set2 = set(int(i) for i in input().split())
+set3 = set(int(i) for i in input().split())
+print(*sorted((set1 | set2 | set3)-(set1 | set2), reverse = True))
