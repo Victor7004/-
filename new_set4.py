8a75d3d6b0dd246bce63b955c8c9446f1d0c6f31
Victor7004 <viktoraleksejcuk@gmail.com>
2022-05-25 16:43:16 +0300

    Create new_set4.py

---
+++ new_set4.py
@@ -0,0 +1,10 @@
+# Даны по 1010-балльной шкале оценки по биологии трех учеников. Напишите программу, которая выводит множество оценок, не встречающихся ни у одного из трех учеников.
+
+set1 = set(int(i) for i in input().split())
+set2 = set(int(i) for i in input().split())
+set3 = set(int(i) for i in input().split())
+set4 = set()
+for i in range(11):
+    set4.add(i)
+#print(set4)
+print(*sorted((set4)-(set1 | set2 | set3)))
