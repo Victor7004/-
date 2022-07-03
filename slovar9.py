d312c05187c6aac2f7ad555fcb0a9c20760423d6
Victor7004 <viktoraleksejcuk@gmail.com>
2022-07-03 15:56:09 +0300

    Update slovar9.py

--- slovar9.py
+++ slovar9.py
@@ -10,3 +10,14 @@ for key, values in sorted(result.items()):
     if values == min(result.values()):
         print(key)
         break
+        
+# Верное решение #443948930
+# Python 3
+# put your python code here
+dct = {}
+lst = [word.strip('.,!?:;-') for word in input().lower().split()]
+for word in lst:
+    dct[word] = dct.get(word, 0) + 1
+lst = [(value, key) for key, value in dct.items()]
+lst.sort()
+print(lst[0][1])
