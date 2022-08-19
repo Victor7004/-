109dd0f30de7312fb4fc4b02f070c9c475f2e625
Victor7004 <viktoraleksejcuk@gmail.com>
2022-08-19 20:08:11 +0300

    Create pth6.py

---
+++ pth6.py
@@ -0,0 +1,8 @@
+# Дан список чисел от 0 до 10. На вход подается целое число. Если это число есть в списке, верните True, иначе верните False.
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n = int(input())
+#if n in numbers:
+    #print('True')
+#else:
+    #print('False')
+print(n in numbers)
