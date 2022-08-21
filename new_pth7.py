fc0081d31c10026938ff5a57c2f26387cf518933
Victor7004 <viktoraleksejcuk@gmail.com>
2022-08-21 13:23:48 +0300

    Create new_pth7.py

---
+++ new_pth7.py
@@ -0,0 +1,25 @@
+# Напишите программу для расшифровки секретного слова методом частотного анализа.
+s = input()
+n = int(input())
+s2 = []
+#for i in range(n)
+c = {}
+c1 = {}
+for i in range(n):
+    value, key = input().split(": ")
+    c.setdefault(value, int(key))    #value, key = input().split(": "), при добавлении в словарь ключ привести к целому int(key)
+    #s1 = input().split()
+    #s2.append(s1)
+c1 = {v: k for k,v in c.items()}
+#print(c1)    
+#print(s, n, c )
+for j in range(len(s)):
+    key = s.count(s[j])
+    print(c1[key], end = '')
+#1.создаём словарь с цифрами в качестве ключей и буквами в качестве значений
+
+#2.пускаем цикл с range(длина вводимой строки)
+
+#3.в цикле получаем значения нашего словаря по ключу ваша_строка.count(ваша_строка[i]) и выводим их с параметром end="" 
+
+#Удачи! =)
