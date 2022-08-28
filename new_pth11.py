1157806088a93a06e72c1a4c6a1fed19eb812ba6
Victor7004 <viktoraleksejcuk@gmail.com>
2022-08-28 13:02:29 +0300

    Update new_pth11.py

--- new_pth11.py
+++ new_pth11.py
@@ -7,3 +7,8 @@ result = {}
 #result = {i: months[i] for i in months months[i] = i }
 for i in months:
     result[months[i]] = i
+# Верное решение #431844113
+# Python 3
+months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+result = {v: k for k, v in months.items()}
