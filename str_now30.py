07199ed9b12bebb7cead6d02956c5aa94989f269
Victor7004 <viktoraleksejcuk@gmail.com>
2023-01-14 20:07:42 +0200

    Create str_now30.py

---
+++ str_now30.py
@@ -0,0 +1,2 @@
+#  Используя raw-строки, задайте строку, содержащую этот путь к файлу: C:\WINDOWS\System32\drivers\etc\hosts. Результат отобразите на экране.
+print(r"C:\WINDOWS\System32\drivers\etc\hosts")
